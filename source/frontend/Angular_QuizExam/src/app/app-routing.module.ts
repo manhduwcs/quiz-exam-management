5ef583ce963ccda21d305f7cd5ac94e7c0fa49ac
Minh LD <minh.ld.2187@aptechlearning.edu.vn>
2026-02-27 15:09:06 +0700

    security

--- source/frontend/Angular_QuizExam/src/app/app-routing.module.ts
+++ source/frontend/Angular_QuizExam/src/app/app-routing.module.ts
@@ -1,15 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './student/login/login.component';
-import { HeaderComponent } from './student/home/common/header/header.component';
-import { FooterComponent } from './student/home/common/footer/footer.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: '/header', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'header', component: HeaderComponent },
-  { path: 'footer', component: FooterComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
