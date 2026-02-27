a20e47b1398d2d3240eeb302356a03f556052ce3
Anh NH <anh.nh.960@aptechlearning.edu.vn>
2026-02-27 09:24:51 +0700

    demo security

--- source/frontend/Angular_QuizExam/src/app/app.module.server.ts
+++ source/frontend/Angular_QuizExam/src/app/app.module.server.ts
@@ -4,7 +4,6 @@ import { ServerModule } from '@angular/platform-server';
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 
-
 @NgModule({
   imports: [
     AppModule,
