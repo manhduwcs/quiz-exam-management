bdb5210439d3a81db3d1ffd393b9b1c945330df7
Thanh Nam <thanhnam2425@gmail.com>
2026-02-27 12:47:47 +0700

    update header, footer

--- source/frontend/Angular_QuizExam/src/app/student/home/common/common.module.ts
+++ source/frontend/Angular_QuizExam/src/app/student/home/common/common.module.ts
@@ -1,19 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// import { HeaderComponent } from './header/header.component';
-// import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    // HeaderComponent,
-    // FooterComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
   ]
 })
-export class ImportModule {
-  
- }
+export class CommonModule { }
