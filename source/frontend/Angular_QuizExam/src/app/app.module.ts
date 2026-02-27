ccc046256152e2d6145c4280fd411ff471578f2c
Minh LD <minh.ld.2187@aptechlearning.edu.vn>
2026-02-27 14:46:00 +0700

    security

--- source/frontend/Angular_QuizExam/src/app/app.module.ts
+++ source/frontend/Angular_QuizExam/src/app/app.module.ts
@@ -6,7 +6,8 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './student/login/login.component';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
