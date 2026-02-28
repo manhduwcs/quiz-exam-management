4917109091dc515dee084c88f7fe3d4d26bfedb5
Manh Nguyen <manhnguyenn23077@gmail.com>
2026-02-28 18:54:03 +0700

    fix dark mode

--- source/frontend/Angular_QuizExam/src/app/student/home/profile/profile.component.ts
+++ source/frontend/Angular_QuizExam/src/app/student/home/profile/profile.component.ts
@@ -6,11 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
-  visiblePassword(value: string): void {
-    const passwordInput = document.getElementById(value) as HTMLInputElement;
-    if (passwordInput) {
-      const type = passwordInput.type === "password" ? "text" : "password";
-      passwordInput.type = type;
-    }
-  }
+
 }
