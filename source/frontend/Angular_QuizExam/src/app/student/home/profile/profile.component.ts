238a88a1a9614301dc371a4646e9f51066ee6872
Thanh Nam <thanhnam2425@gmail.com>
2026-03-01 08:46:30 +0700

    btn logout

--- source/frontend/Angular_QuizExam/src/app/student/home/profile/profile.component.ts
+++ source/frontend/Angular_QuizExam/src/app/student/home/profile/profile.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { profile } from 'node:console';
 
 @Component({
   selector: 'app-profile',
@@ -7,18 +6,11 @@ import { profile } from 'node:console';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
-  // Hàm để ẩn/hiện mật khẩu
-  visiblePassword(inputId: string): void {
-    // Tìm phần tử đầu vào mật khẩu bằng ID
-    const passwordInput = document.getElementById(inputId) as HTMLInputElement;
-
+  visiblePassword(value: string): void {
+    const passwordInput = document.getElementById(value) as HTMLInputElement;
     if (passwordInput) {
-      // Kiểm tra và thay đổi thuộc tính 'type' giữa 'password' và 'text'
-      const type = passwordInput.getAttribute("type") === "password" ? "text" : "password";
-      passwordInput.setAttribute("type", type);
-    } else {
-      console.error(`Element with id ${inputId} not found.`);
+      const type = passwordInput.type === "password" ? "text" : "password";
+      passwordInput.type = type;
     }
   }
 }
-
