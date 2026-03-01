965e9c1238bd4c129cbece9f259c956650d90fc4
Manh Nguyen <manhnguyenn23077@gmail.com>
2026-03-02 01:13:04 +0700

    update popup employee

--- source/frontend/Angular_QuizExam/src/app/admin/login/login.component.ts
+++ source/frontend/Angular_QuizExam/src/app/admin/login/login.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Injectable, OnInit } from '@angular/core';
+// import 'swiper/swiper-bundle.css'; // Thêm dòng này vào
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
 
+export class LoginComponent {
 }
+
