a42bf578dab4d137bab9efbd8c2826f503c856d2
Manh Nguyen <manhnguyenn23077@gmail.com>
2026-03-01 16:53:28 +0700

    login admin

--- source/frontend/Angular_QuizExam/src/app/admin/admin.module.ts
+++ source/frontend/Angular_QuizExam/src/app/admin/admin.module.ts
@@ -1,19 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
-import { AdminRoutingModule } from './admin-routing.module';
-import { AdminComponent } from './admin.component';
 
 
 
 @NgModule({
   declarations: [
-    LoginComponent,
-    AdminComponent
+    LoginComponent
   ],
   imports: [
-    CommonModule,
-    AdminRoutingModule
+    CommonModule
   ]
 })
 export class AdminModule { }
