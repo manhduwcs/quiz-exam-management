132e4d03f5bdf9bcbb3dd41898c6f0ab2a2a354f
Manh Nguyen <manhnguyenn23077@gmail.com>
2026-03-03 13:54:50 +0700

    login student validator

--- source/frontend/Angular_QuizExam/src/app/admin/home/student/student.component.ts
+++ source/frontend/Angular_QuizExam/src/app/admin/home/student/student.component.ts
@@ -18,7 +18,6 @@ export class StudentComponent implements OnInit, OnDestroy {
   stdDetail: any = null;
   isPopupDetail = false;
   isPopupCreate = false;
-  isPopupMove = false;
 
   ngOnInit(): void {
     this.http.get<any>(`${this.authService.apiUrl}/user`).subscribe((data: any) => {
@@ -77,10 +76,6 @@ export class StudentComponent implements OnInit, OnDestroy {
         $('.create').on('click', () => {
           this.isPopupCreate = true;
         });
-
-        $('.move').on('click', () => {
-          this.isPopupMove = true;
-        });
       }
     });
   }
@@ -126,30 +121,6 @@ export class StudentComponent implements OnInit, OnDestroy {
       },
       error => {
         this.toastr.error('Error create Employee', 'Error', {
-          timeOut: 2000,
-        });
-        console.log('Error', error);
-      }
-    )
-  }
-
-  class: String = '';
-  moveStudent(): void {
-    const classes =
-    {
-      class: this.class
-    }
-
-    this.http.post(`${this.authService.apiUrl}/auth/register`, classes, { responseType: 'json' }).subscribe(
-      response => {
-        this.toastr.success('Move Successful!', 'Success', {
-          timeOut: 2000,
-        });
-        console.log('Move successfully', response);
-        this.router.navigate(['/admin/home/student']);
-      },
-      error => {
-        this.toastr.error('Error ', 'Error', {
           timeOut: 2000,
         });
         console.log('Error', error);
