68111fa1e9298a860fa38a12c548de2c80c42bb7
Thanh Nam <thanhnam2425@gmail.com>
2026-03-04 00:22:35 +0700

    fe list + popup subject

--- source/backend/QuizExam_Student/src/main/java/com/example/quizexam_student/service/ExportService.java
+++
@@ -1,9 +0,0 @@
-package com.example.quizexam_student.service;
-
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface ExportService {
-    void export(HttpServletResponse response, String entity, String type);
-}
