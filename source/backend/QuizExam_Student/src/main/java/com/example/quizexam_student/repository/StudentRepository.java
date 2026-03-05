77ceb57f2ecb813cac456ef05afebda008a85e20
Thanh Nam <thanhnam2425@gmail.com>
2026-03-05 13:05:06 +0700

    api chapter crud

--- source/backend/QuizExam_Student/src/main/java/com/example/quizexam_student/repository/StudentRepository.java
+++ source/backend/QuizExam_Student/src/main/java/com/example/quizexam_student/repository/StudentRepository.java
@@ -6,12 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface StudentRepository extends JpaRepository<StudentDetail, Integer> {
     StudentDetail findStudentDetailByUser(User user);
-
     StudentDetail findByUser(User user);
 
     boolean existsByRollPortal(String rollPortal);
@@ -21,6 +18,4 @@ public interface StudentRepository extends JpaRepository<StudentDetail, Integer>
     boolean existsByRollPortalAndUserNot(String rollPortal, User user);
 
     boolean existsByRollNumberAndUserNot(String rollNumber, User user);
-
-    List<StudentDetail> findAllByUserIdIn(List<Integer> userIds);
 }
