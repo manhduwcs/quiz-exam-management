c8d91d0f44514f10a681a6f09eb00d9201d9eed4
Thanh Nam <thanhnam2425@gmail.com>
2026-03-05 15:30:53 +0700

    admin-subject

--- source/backend/QuizExam_Student/src/main/java/com/example/quizexam_student/config/SecurityConfig.java
+++ source/backend/QuizExam_Student/src/main/java/com/example/quizexam_student/config/SecurityConfig.java
@@ -45,7 +45,6 @@ public class SecurityConfig {
                 .requestMatchers("/api/class/**").hasAnyRole("ADMIN", "DIRECTOR", "SRO")
                 .requestMatchers("/api/subject/**").hasAnyRole("ADMIN", "DIRECTOR")
                 .requestMatchers("/api/studentManagement/**").hasAnyRole("ADMIN", "SRO")
-                .requestMatchers("/api/chapter/**").permitAll()
                 .anyRequest().authenticated()
                 
                 .and()
