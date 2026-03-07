0d3e65938867ce771fc8636f0a94c7bf52126faa
Manh Nguyen <manhnguyenn23077@gmail.com>
2026-03-07 23:19:18 +0700

    fix create question

--- source/backend/QuizExam_Student/src/main/java/com/example/quizexam_student/service/QuestionService.java
+++ source/backend/QuizExam_Student/src/main/java/com/example/quizexam_student/service/QuestionService.java
@@ -5,10 +5,12 @@ import com.example.quizexam_student.bean.response.QuestionResponse;
 import com.example.quizexam_student.entity.Question;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
 public interface QuestionService {
     List<QuestionResponse> getAllQuestionsBySubjectId(int subjectId);
-    Question saveQuestion(QuestionRequest questionRequest);
+    List<Question> saveQuestions(List<QuestionRequest> questionRequests) throws IOException;
+    Question updateQuestion(int id, QuestionRequest questionRequest) throws IOException;
 }
