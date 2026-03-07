6124367e0ce0e51260efd29e603ce2314ec3a96b
Manh Nguyen <manhnguyenn23077@gmail.com>
2026-03-07 20:42:06 +0700

    update question-form

--- source/backend/QuizExam_Student/src/main/java/com/example/quizexam_student/service/QuestionService.java
+++ source/backend/QuizExam_Student/src/main/java/com/example/quizexam_student/service/QuestionService.java
@@ -5,12 +5,10 @@ import com.example.quizexam_student.bean.response.QuestionResponse;
 import com.example.quizexam_student.entity.Question;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.util.List;
 
 @Service
 public interface QuestionService {
     List<QuestionResponse> getAllQuestionsBySubjectId(int subjectId);
-    Question saveQuestion(QuestionRequest questionRequest) throws IOException;
-    Question updateQuestion(int id, QuestionRequest questionRequest) throws IOException;
+    Question saveQuestion(QuestionRequest questionRequest);
 }
