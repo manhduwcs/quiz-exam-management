bc15380fa3d11be3797204f72d1b1fc525c74b23
Minh LD <minh.ld.2187@aptechlearning.edu.vn>
2026-03-11 18:48:30 +0700

    generate exam

--- source/backend/QuizExam_Student/src/main/java/com/example/quizexam_student/repository/StudentAnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.quizexam_student.repository;
-
-import com.example.quizexam_student.entity.StudentAnswer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentAnswerRepository extends JpaRepository<StudentAnswer, Integer> {
-}
