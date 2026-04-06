85eeaa1d4a6fbe5ad3c88d2c51e80cec299db154
manhduwcs <manhnguyenn23077@gmail.com>
2026-04-06 19:01:45 +0700

    refractor frontend codebase

--- source/v2/src/app/admin/home/subject/subject.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-subject',
-  templateUrl: './subject.component.html',
-})
-export class SubjectComponent {
-  constructor() { }
-}
